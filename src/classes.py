bcf84a148e76895349c144f9631e8b31436bf5cd
Nicolas Ibach <nicolas@Mac.fritz.box>
2026-05-10 12:00:51 +0200

    Initial commit - rough scaffolding

---
+++ src/classes.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class AgentState(BaseModel):
+    pass
---
+++ src/nodes.py
@@ -0,0 +1,24 @@
+def pick_up_ticket():
+    pass
+
+def spec():
+    pass
+
+def open_branch():
+    pass
+
+def write_tests():
+    pass
+
+def write_code():
+    pass
+
+def review():
+    pass
+
+def commit_push():
+    pass
+
+def merge():
+    pass
+
